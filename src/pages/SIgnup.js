11817add0a97199ebc795ad9755d4e35bbd08ca8
Brian Wilson <bmwilson93@gmail.com>
2024-12-06 05:18:20 -0700

    added username state , input, and handler, and changes log in to say sign up

--- src/pages/SIgnup.js
+++ src/pages/SIgnup.js
@@ -4,16 +4,18 @@ import './css/Login.css';
 const Signup = () => {
   // Form Input States
   const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
   // Form Input Handlers
   const handleEmailChange = e => setEmail(e.target.value);
+  const handleUsernameChange = e => setUsername(e.target.value);
   const handlePasswordChange = e => setPassword(e.target.value);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     // build the body object
-    // make the call with a login util function
+    // make the call with a signup util function
     // set the user State (passed from App.js)
     // clear the password state
   }
@@ -22,14 +24,15 @@ const Signup = () => {
   return (
     <div className='full-height'>
 
-      <div className="login-container">
-        <h2>Log In</h2>
-        <form>
+      <div className="signup-container">
+        <h2>Sign Up</h2>
+        <form onSubmit={handleSubmit}>
           <input type='email' value={email} onChange={handleEmailChange} placeholder='Email'/>
+          <input type='text' value={username} onChange={handleUsernameChange} placeholder='Username'/>
           <input type='password' value={password} onChange={handlePasswordChange} placeholder='Password'/>
           <button onClick={handleSubmit}>Log In</button>
         </form>
-        <p>Don't have an account? <a>Sign Up</a></p>
+        <p>Already have an account? <a>Log In</a></p>
       </div>
       
     </div>
