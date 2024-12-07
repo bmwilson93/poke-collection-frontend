6607b07a17686803a3ec836e0fd1243f39e4c82a
Brian Wilson <bmwilson93@gmail.com>
2024-12-07 03:09:41 -0700

    added useNavigate and navigation to login page, completed handleSubmit function to register new user

--- src/pages/SIgnup.js
+++ src/pages/SIgnup.js
@@ -1,7 +1,10 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { fetchAPI } from "../utils/fetchAPI";
 import './css/Login.css';
 
-const Signup = () => {
+const Signup = ( setUser ) => {
+  const navigate = useNavigate();
   // Form Input States
   const [email, setEmail] = useState('');
   const [username, setUsername] = useState('');
@@ -15,9 +18,14 @@ const Signup = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     // build the body object
-    // make the call with a login util function
-    // set the user State (passed from App.js)
-    // clear the password state
+    let body = JSON.stringify({
+      "email": email,
+      "username": username,
+      "password": password
+    })
+    const response = await fetchAPI('register', 'POST', body);
+    setUsername(response);
+    setPassword('');
   }
 
 
@@ -32,7 +40,7 @@ const Signup = () => {
           <input type='password' value={password} onChange={handlePasswordChange} placeholder='Password'/>
           <button onClick={handleSubmit}>Sign Up</button>
         </form>
-        <p>Already have an account? <a>Log In</a></p>
+        <p>Already have an account? <a onClick={() => {navigate('/login')}}>Log In</a></p>
       </div>
       
     </div>
