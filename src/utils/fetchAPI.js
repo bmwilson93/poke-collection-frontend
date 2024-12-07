20c091c2bf1f1e6563c1bbd402622acea113040f
Brian Wilson <bmwilson93@gmail.com>
2024-12-07 03:09:02 -0700

    created fetchAPI, added function to make calls to backend express server

---
+++ src/utils/fetchAPI.js
@@ -0,0 +1,25 @@
+const fetchAPI = async (route, method, body) => {
+  console.log('fetchAPI')
+  try {
+    const result = await fetch(`${process.env.REACT_APP_API_PATH}/${route}`, {
+      method: method,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: body,
+      credentials: "include"
+    });
+    if (result.ok) {
+      const data = await result.json();
+      console.log("data:")
+      console.log(data);
+      return data;
+    }
+    return null;
+  } catch (error) {
+    console.log(error);
+    return null
+  }
+}
+
+module.exports = {fetchAPI}
