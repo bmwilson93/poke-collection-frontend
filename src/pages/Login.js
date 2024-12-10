04bcce681f715ca8ab9c37da7b80ad1200f8c1da
Brian Wilson <bmwilson93@gmail.com>
2024-12-10 02:51:30 -0700

    Added redirect to home page after login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -23,6 +23,7 @@ const Login = ({ setUser, applyCollected }) => {
     const response = await fetchAPI('login', 'POST', body);
     setUser(response);
     setPassword('');
+    navigate('/');
   }
 
 
