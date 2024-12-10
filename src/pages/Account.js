faf39ce4c620f768fad2b59d060c4956feb9cc6e
Brian Wilson <bmwilson93@gmail.com>
2024-12-10 02:51:11 -0700

    added logout function and a check to only display user info if user is logged in

--- src/pages/Account.js
+++ src/pages/Account.js
@@ -1,8 +1,9 @@
 import React, { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom';
+import { fetchAPI } from "../utils/fetchAPI";
 import './css/Account.css';
 
-const Account = ({ user }) => {
+const Account = ({ user, setUser }) => {
   const navigate = useNavigate();
 
   // On load, check if logged in
@@ -12,11 +13,33 @@ const Account = ({ user }) => {
     }
   }, [])
 
+  const handleLogout = async (e) => {
+    e.preventDefault();
+    await fetchAPI('logout', 'POST', "");
+    setUser(null)
+    navigate('/');
+  }
+
   return (
     <div className='full-height'>
+    {user 
+      ? <div>
+          <div>
+            <h2>{user.username}</h2>
+            <p>{user.email}</p>
+          </div>
 
-      Account
+          <div>
+            <h3>Your Collection:</h3>
+          </div>
 
+          <div>
+            <p onClick={handleLogout}>Log OUt</p>
+          </div>
+        </div>
+      : <></>
+    }
+    
     </div>
   )
 }
