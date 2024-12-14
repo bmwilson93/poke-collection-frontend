f2e3a8aa388e195e81beebe4a855296c70417a02
Brian Wilson <bmwilson93@gmail.com>
2024-12-14 13:11:24 -0700

    updated to display differnt card list for selected filter

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,60 +1,52 @@
 import React from 'react'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import CardItem from './CardItem'
 import './css/CardList.css';
 
 import checkmark from '../assets/check-circle-solid-36.png'
-import { render } from '@testing-library/react';
 
 const CardList = ({ cards }) => {
   const navigate = useNavigate();
+  const [filterState, setFilterState] = useState('all');
 
-  const [filterState, setFilterState] = useState('all')
-
+  const makeCard = (card) => {
+    return (<li key={card.id} onClick={() => navigate(`/card/${card.id}`, {state:{card: card}})}>
+      
+    {card.collected ? <img src={checkmark} className='checkmark-list' /> : <></>}
+    {card.collectedQuantity}
+    
+    <CardItem name={card.name} 
+      setName={card.set.name} 
+      avgSellPrice={card.cardmarket && card.cardmarket.prices && card.cardmarket.prices.averageSellPrice}
+      imgSrc={card.images.small}/>
+  </li>);
+  }
+  
   // save collected and notCollected my filtering cards
   const collectedCards = cards.filter(card => Object.hasOwn(card, "collected"));
-
   const notCollectedCards = cards.filter(card => !Object.hasOwn(card, "collected"));
 
-  // then take the list of cards var and turn it into a function that takes cards as arg
-  const renderCards = (cards) => {cards.map(card => 
-    <li key={card.id} onClick={() => navigate(`/card/${card.id}`, {state:{card: card}})}>
-      
-      {card.collected ? <img src={checkmark} className='checkmark-list' /> : <></>}
-      {card.collectedQuantity}
-      
-      <CardItem name={card.name} 
-        setName={card.set.name} 
-        avgSellPrice={card.cardmarket && card.cardmarket.prices && card.cardmarket.prices.averageSellPrice}
-        // tcgAvgSellPrice={card.tcgplayer && card.tcgplayer.prices && card.tcgplayer.prices.holofoil || card.tcgplayer.prices.reverseHolofoil || card.tcgplayer.prices.normal}
-        imgSrc={card.images.small}/>
-    </li>)
-  }
-
-  // then call the function from the component, and view only what is selected
-  // use a state to manage which filter is selected and shown
-
   // Renders a list of cards that is stored in a cards state the App.js file
-  // const listOfCards = cards.map(card => 
-  //   <li key={card.id} onClick={() => navigate(`/card/${card.id}`, {state:{card: card}})}>
-      
-  //     {card.collected ? <img src={checkmark} className='checkmark-list' /> : <></>}
-  //     {card.collectedQuantity}
-      
-  //     <CardItem name={card.name} 
-  //       setName={card.set.name} 
-  //       avgSellPrice={card.cardmarket && card.cardmarket.prices && card.cardmarket.prices.averageSellPrice}
-  //       // tcgAvgSellPrice={card.tcgplayer && card.tcgplayer.prices && card.tcgplayer.prices.holofoil || card.tcgplayer.prices.reverseHolofoil || card.tcgplayer.prices.normal}
-  //       imgSrc={card.images.small}/>
-  //   </li>
-  // )
+  const listOfCards = cards.map(card => makeCard(card))
+  const listOfCardsCollected = collectedCards.map(card => makeCard(card))
+  const listOfCardsNotCollected = notCollectedCards.map(card => makeCard(card))
 
   return (
     <div className="card-list-container">
 
+      <div className='filter-container'>
+        <button onClick={() => setFilterState('all')} className={filterState === 'all' ? 'selected' : ''}>All Cards</button>
+        <button onClick={() => setFilterState('collected')} className={filterState === 'collected' ? 'selected' : ''}>Collected</button>
+        <button onClick={() => setFilterState('notCollected')} className={filterState === 'notCollected' ? 'selected' : ''}>Not Collected</button>
+      </div>
+
       <ul className="card-list">
-        {renderCards(cards)}
+        {filterState === 'all' 
+          ? listOfCards
+          : filterState === 'collected'
+            ? listOfCardsCollected
+            : listOfCardsNotCollected}
       </ul>
 
     </div>
