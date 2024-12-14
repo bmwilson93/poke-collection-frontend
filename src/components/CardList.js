3e511c015b20e40399d79c1cc6fb1532c80d7968
Brian Wilson <bmwilson93@gmail.com>
2024-12-14 02:04:43 -0700

    moved the cardlist var into a function to build a list to render in the ul element.

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -4,6 +4,7 @@ import CardItem from './CardItem'
 import './css/CardList.css';
 
 import checkmark from '../assets/check-circle-solid-36.png'
+import { render } from '@testing-library/react';
 
 const CardList = ({ cards }) => {
   const navigate = useNavigate();
@@ -14,11 +15,7 @@ const CardList = ({ cards }) => {
   const notCollectedCards = cards.filter(card => !Object.hasOwn(card, "collected"));
 
   // then take the list of cards var and turn it into a function that takes cards as arg
-  // then call the function from the component, and view only what is selected
-  // use a state to manage which filter is selected and shown
-
-  // Renders a list of cards that is stored in a cards state the App.js file
-  const listOfCards = cards.map(card => 
+  const renderCards = (cards) => {cards.map(card => 
     <li key={card.id} onClick={() => navigate(`/card/${card.id}`, {state:{card: card}})}>
       
       {card.collected ? <img src={checkmark} className='checkmark-list' /> : <></>}
@@ -29,14 +26,32 @@ const CardList = ({ cards }) => {
         avgSellPrice={card.cardmarket && card.cardmarket.prices && card.cardmarket.prices.averageSellPrice}
         // tcgAvgSellPrice={card.tcgplayer && card.tcgplayer.prices && card.tcgplayer.prices.holofoil || card.tcgplayer.prices.reverseHolofoil || card.tcgplayer.prices.normal}
         imgSrc={card.images.small}/>
-    </li>
-  )
+    </li>)
+  }
+
+  // then call the function from the component, and view only what is selected
+  // use a state to manage which filter is selected and shown
+
+  // Renders a list of cards that is stored in a cards state the App.js file
+  // const listOfCards = cards.map(card => 
+  //   <li key={card.id} onClick={() => navigate(`/card/${card.id}`, {state:{card: card}})}>
+      
+  //     {card.collected ? <img src={checkmark} className='checkmark-list' /> : <></>}
+  //     {card.collectedQuantity}
+      
+  //     <CardItem name={card.name} 
+  //       setName={card.set.name} 
+  //       avgSellPrice={card.cardmarket && card.cardmarket.prices && card.cardmarket.prices.averageSellPrice}
+  //       // tcgAvgSellPrice={card.tcgplayer && card.tcgplayer.prices && card.tcgplayer.prices.holofoil || card.tcgplayer.prices.reverseHolofoil || card.tcgplayer.prices.normal}
+  //       imgSrc={card.images.small}/>
+  //   </li>
+  // )
 
   return (
     <div className="card-list-container">
 
       <ul className="card-list">
-        {listOfCards}
+        {renderCards(cards)}
       </ul>
 
     </div>
