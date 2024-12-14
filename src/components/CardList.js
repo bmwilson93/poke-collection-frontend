d489af88e11855991379a3e529746cb998f299a3
Brian Wilson <bmwilson93@gmail.com>
2024-12-14 01:51:17 -0700

    Added variables to hold filtered cards, for collected and not collected

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -8,6 +8,15 @@ import checkmark from '../assets/check-circle-solid-36.png'
 const CardList = ({ cards }) => {
   const navigate = useNavigate();
 
+  // save collected and notCollected my filtering cards
+  const collectedCards = cards.filter();
+
+  const notCollectedCards = cards.filter();
+  
+  // then take the list of cards var and turn it into a function that takes cards as arg
+  // then call the function from the component, and view only what is selected
+  // use a state to manage which filter is selected and shown
+
   // Renders a list of cards that is stored in a cards state the App.js file
   const listOfCards = cards.map(card => 
     <li key={card.id} onClick={() => navigate(`/card/${card.id}`, {state:{card: card}})}>
