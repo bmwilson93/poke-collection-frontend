584a5bbfbf1691180b2e9cb4c7e5fa368b4261ef
Brian Wilson <bmwilson93@gmail.com>
2025-01-18 05:04:33 -0700

    Removed old code

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -10,45 +10,7 @@ const CardList = ({ filterState, setFilterState, cards, user, scrollValue, setSc
   const navigate = useNavigate();
   const [mappedCards, setMappedCards] = useState([]);
   const [cardSort, setCardSort] = useState('number');
-
-
-  // TODO DELTE BELOW HERE --
-  const makeCard = (card) => {
-    return (<li key={card.id} onClick={() => {
-      setScrollValue(window.scrollY);
-      navigate(`/card/${card.id}`, {state:{card: card}})}
-    }>
-      
-    {card.collected ? <img src={checkmark} className='checkmark-list' /> : <></>}
-    {card.collectedQuantity}
-    
-    <CardItem name={card.name} 
-      setName={card.set.name} 
-      avgSellPrice={card.cardmarket && card.cardmarket.prices && card.cardmarket.prices.averageSellPrice}
-      imgSrc={card.images.small}/>
-  </li>);
-  }
   
-  // TODO, add states to hold collected and notCollected
-  // save collected and notCollected my filtering cards
-  const collectedCards = cards.filter(card => Object.hasOwn(card, "collected"));
-  const notCollectedCards = cards.filter(card => !Object.hasOwn(card, "collected"));
-
-  // Renders a list of cards that is stored in a cards state the App.js file
-  
-  const doCards = () => {
-    let retCard = cards.map(card => makeCard(card));
-    window.scrollTo(0, scrollValue);
-    return retCard;
-  }
-
-  // TODO, currently, these arrays are calculated once, not dynamicaly
-  // need to set a function that maps the correct card list based on both filter and sort
-  // and save in a state
-  const listOfCards = doCards();
-  const listOfCardsCollected = collectedCards.map(card => makeCard(card))
-  const listOfCardsNotCollected = notCollectedCards.map(card => makeCard(card))
-  // TODO DELETE ABOVE HERE --
 
   // Set to saved scroll on page load
   useEffect(() => {
@@ -75,7 +37,6 @@ const CardList = ({ filterState, setFilterState, cards, user, scrollValue, setSc
   }
 
 
-  // WORK IN PROGRESS
   const handleDisplayCardlist = () => {
     // check the filter
     let filteredCards = [];
@@ -122,7 +83,6 @@ const CardList = ({ filterState, setFilterState, cards, user, scrollValue, setSc
       : <></>
       }
       
-      {/* TODO, update to display the mappedCards state */}
       <ul className="card-list">
         {mappedCards}
       </ul>
