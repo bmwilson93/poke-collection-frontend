861a541cd7248dc17a47a93f5559f9b3911a5060
Brian Wilson <bmwilson93@gmail.com>
2025-01-23 22:48:56 -0700

    Added function to display cardmarket prices, and tcgplayer prices if cardmarket property is undefined

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -1,7 +1,24 @@
 import React from 'react';
 import './css/CardItem.css';
 
-const CardItem = ({ name, setName, avgSellPrice, tcgAvgSellPrice, imgSrc}) => {
+const CardItem = ({ name, setName, avgSellPrice, tcgPrices, imgSrc}) => {
+  const displayAveragePrice = () => {
+    if (avgSellPrice) {
+      return avgSellPrice.toFixed(2);
+    } else { // No cardMarkent Avg price, check for tcgplayer prices instead
+      if (tcgPrices?.normal) {
+        return tcgPrices.normal.market.toFixed(2);
+      } else if (tcgPrices?.holofoil) {
+        return tcgPrices.holofoil.market.toFixed(2);
+      } else if (tcgPrices?.reverseHolofoil) {
+        return tcgPrices.reverseHolofoil.market.toFixed(2);
+      } else {
+        return 'NA'
+      }
+    }
+  }
+
+
   return (
     <div
       className="card-item hover-grow"
@@ -10,9 +27,7 @@ const CardItem = ({ name, setName, avgSellPrice, tcgAvgSellPrice, imgSrc}) => {
         className="cardlist-image"
         loading='lazy'
       />
-      <span className="avg-price">Market Average: ${
-      avgSellPrice ? avgSellPrice.toFixed(2) : 'NA'
-      }</span>
+      <span className="avg-price">Market Average: ${displayAveragePrice()}</span>
     </div>
   )
 }
