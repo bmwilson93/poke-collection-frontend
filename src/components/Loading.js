ebc25ba40dbc4d7885d85d83b60ac44e018eb69e
Brian Wilson <bmwilson93@gmail.com>
2025-01-23 23:27:06 -0700

    Removed the inline styles

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -4,11 +4,7 @@ import './css/Loading.css'
 
 const Loading = () => {
   return (
-    <div className="loading"
-      style={{
-        height: '80vh',
-      }}
-    >
+    <div className="loading">
       <div className="lds-ring"><div></div><div></div><div></div><div></div></div>
     </div>
   )
