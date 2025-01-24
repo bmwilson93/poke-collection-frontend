808976845720b91e961c1e829c0db8451f1f5c3e
Brian Wilson <bmwilson93@gmail.com>
2025-01-23 23:25:55 -0700

    Moved the cardList component to only render if we have cards, and added the full-height className to the container div

--- src/pages/SearchResult.js
+++ src/pages/SearchResult.js
@@ -5,19 +5,22 @@ import Loading from '../components/Loading'
 
 const SearchResult = ({ cardSort, setCardSort, filterState, setFilterState, scrollValue, setScrollValue, cards, currentPage, pages, getCards, recentSearch, cardCount, user}) => {
   return (
-    <div className="search-results-container">
-      <CardList cardSort={cardSort} setCardSort={setCardSort} filterState={filterState} setFilterState={setFilterState} scrollValue={scrollValue} setScrollValue={setScrollValue}cards={cards} user={user}/>
-      
+    <div className="search-results-container full-height">
+
       {(() => {
-        if (cardCount == -1) {
-          return <Loading />
+      if (cardCount == -1) {
+        return <Loading />
         } else if (cardCount == 0) {
           return <div className="not-found-message">No Cards Found...</div>
-        } else {
-          return <PageNavigation currentPage={currentPage} pages={pages} getCards={getCards} recentSearch={recentSearch}/>
+          } else {
+          return (
+            <>
+              <CardList cardSort={cardSort} setCardSort={setCardSort} filterState={filterState} setFilterState={setFilterState} scrollValue={scrollValue} setScrollValue={setScrollValue}cards={cards} user={user}/>
+              <PageNavigation currentPage={currentPage} pages={pages} getCards={getCards} recentSearch={recentSearch}/>
+            </>
+          )
         }
       })()}
-
       
     </div>
   )
