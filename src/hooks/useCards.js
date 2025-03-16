51c58de54447a60d566b2566387cec1d470633b6
Brian Wilson <bmwilson93@gmail.com>
2025-03-16 03:42:10 -0600

    Created useCards function and added states

--- src/hooks/useCards.js
+++ src/hooks/useCards.js
@@ -2,3 +2,9 @@ import { useState, useEffect } from 'react';
 import { getCardsBySearch } from '../utils/fetchData';
 import { setCollected } from '../utils/setcollected';
 
+export const useCards = (user) => {
+  const [cards, setCards] = useState([]);
+  const [cardCount, setCardCount] = useState(-1);
+  const [pages, setPages] = useState(1);
+  const [currentPage, setCurrentPage] = useState(1);
+};
