451e1436f7c86fcb6e60b4592b659cc494086ff8
Brian Wilson <bmwilson93@gmail.com>
2025-03-16 04:07:10 -0600

    Moved applyCollection and getCards functions from App.js

--- src/hooks/useCards.js
+++ src/hooks/useCards.js
@@ -7,4 +7,31 @@ export const useCards = (user) => {
   const [cardCount, setCardCount] = useState(-1);
   const [pages, setPages] = useState(1);
   const [currentPage, setCurrentPage] = useState(1);
+
+  const applyCollection = (cardArray = cards) => {
+    if (user) {
+      let collectedCards = setCollected(cardArray, user.collection);
+      setCards(collectedCards);
+    } else {
+      setCards(cardArray);
+    }
+  };
+
+  const getCards = async (search, page = 1) => {
+    setCards([]);
+    setCardCount(-1);
+
+    const result = await getCardsBySearch(search, page);
+
+    if ('error' in result) {
+      console.error('Error with searching cards.');
+    } else {
+      setCardCount(result.totalCount);
+      setPages(Math.ceil(result.totalCount / result.pageSize));
+      setCurrentPage(result.page);
+      applyCollection(result.data);
+    }
+  };
+
+  return { cards, cardCount, pages, currentPage, getCards, applyCollection };
 };
