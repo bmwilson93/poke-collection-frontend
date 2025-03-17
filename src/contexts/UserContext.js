4a68bb2b3e9bdeb7dc79d1b55f8b44fdcc6c0046
Brian Wilson <bmwilson93@gmail.com>
2025-03-17 01:30:29 -0600

    Created userContext, and added userProvider

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -1,1 +1,14 @@
 import React, { useState } from 'react';
+
+const UserContext = React.createContext();
+
+export const UserProvider = ({ children }) => {
+  
+  return (
+    <UserContext.Provider value={''}>
+      {children}
+    </UserContext.Provider>
+  )
+}
+
+export default UserContext;
