085d9e2ab0b3d2c19079abaa8d488701316330b0
Brian Wilson <bmwilson93@gmail.com>
2025-03-20 02:50:17 -0600

    Added more functions to provier object

--- src/contexts/CardContext.js
+++ src/contexts/CardContext.js
@@ -4,13 +4,13 @@ import { useCards } from '../hooks/useCards';
 const CardContext = React.createContext();
 
 export const CardProvider = ({ children }) => {
-  const { cards, setCards, cardCount, setCardCount, pages, setPages, currentPage, setCurrentPage } = useCards();
+  const { cards, setCards, cardCount, setCardCount, pages, setPages, currentPage, setCurrentPage, getAllSetCards, searchCards } = useCards();
   
   return (
-    <CardContext.Provider value={{ cards, setCards, cardCount, setCardCount, pages, setPages, currentPage, setCurrentPage }}>
+    <CardContext.Provider value={{ cards, setCards, cardCount, setCardCount, pages, setPages, currentPage, setCurrentPage, getAllSetCards, searchCards }}>
       {children}
     </CardContext.Provider>
   );
 };
 
-export default UserContext;
+export default CardContext;
