9fca058627218f768c3bbc8afe1f1c5dd9c55ef0
Brian Wilson <bmwilson93@gmail.com>
2025-03-20 18:04:13 -0600

    Added user prop to CardProvider

--- src/contexts/CardContext.js
+++ src/contexts/CardContext.js
@@ -3,8 +3,8 @@ import { useCards } from '../hooks/useCards';
 
 const CardContext = React.createContext();
 
-export const CardProvider = ({ children }) => {
-  const { cards, setCards, cardCount, setCardCount, pages, setPages, currentPage, setCurrentPage, getAllSetCards, searchCards } = useCards();
+export const CardProvider = ({ user, children }) => {
+  const { cards, setCards, cardCount, setCardCount, pages, setPages, currentPage, setCurrentPage, getAllSetCards, searchCards } = useCards(user);
   
   return (
     <CardContext.Provider value={{ cards, setCards, cardCount, setCardCount, pages, setPages, currentPage, setCurrentPage, getAllSetCards, searchCards }}>
