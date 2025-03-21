d0e1ecd86ba1eb3aaec0e4e6564b7f4a8f2f2a7d
Brian Wilson <bmwilson93@gmail.com>
2025-03-20 22:50:39 -0600

    Updated the CardContext to pass getCards, and put the states and functions on their own lines for readability

--- src/contexts/CardContext.js
+++ src/contexts/CardContext.js
@@ -4,10 +4,32 @@ import { useCards } from '../hooks/useCards';
 const CardContext = React.createContext();
 
 export const CardProvider = ({ user, children }) => {
-  const { cards, setCards, cardCount, setCardCount, pages, setPages, currentPage, setCurrentPage, getAllSetCards, searchCards } = useCards(user);
+  const { 
+    cards, 
+    setCards, 
+    cardCount, 
+    setCardCount, 
+    pages, 
+    setPages, 
+    currentPage, 
+    setCurrentPage, 
+    getAllSetCards, 
+    getCards 
+  } = useCards(user);
   
   return (
-    <CardContext.Provider value={{ cards, setCards, cardCount, setCardCount, pages, setPages, currentPage, setCurrentPage, getAllSetCards, searchCards }}>
+    <CardContext.Provider value={{ 
+      cards, 
+      setCards, 
+      cardCount, 
+      setCardCount, 
+      pages, 
+      setPages, 
+      currentPage, 
+      setCurrentPage, 
+      getAllSetCards, 
+      getCards 
+    }}>
       {children}
     </CardContext.Provider>
   );
