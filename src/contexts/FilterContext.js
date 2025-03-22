a19c6646ee7d420721a602f1c62ecb9e38250d86
Brian Wilson <bmwilson93@gmail.com>
2025-03-22 03:18:56 -0600

    Added filter and sort states for both cards and sets

---
+++ src/contexts/FilterContext.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { useState } from 'react';
+
+const FilterContext = React.createContext();
+
+export const FilterProvider = ({ children }) => {
+  const [cardsFilter, setCardsFilter] = useState('all'); // Filters for card list (IE: collected/not collected)
+  const [cardsSort, setCardsSort] = useState('number'); // Sort options for card list
+  const [setsFilter, setSetsFilter] = useState('All') // Filter for sets by series
+  const [setsSort, setSetsSort] = useState('newest'); // Sort for sets
+  
+  return (
+    <FilterContext.Provider value={{ 
+      cardsFilter,
+      setCardsFilter,
+      cardsSort,
+      setCardsSort,
+      setsFilter, 
+      setSetsFilter,
+      setsSort,
+      setSetsSort
+     }}>
+      {children}
+    </FilterContext.Provider>
+  );
+};
+
+export default FilterContext;
