6202943808013e92f517e7dc85c842147c8d520f
Brian Wilson <bmwilson93@gmail.com>
2025-04-03 01:13:00 -0600

    Updated sortByPrice function to better sort cards using both tcgplayer and cardmarket prices, and handle missing prices

--- src/utils/sortByPrice.js
+++ src/utils/sortByPrice.js
@@ -6,38 +6,110 @@ import { variants } from "./variantList";
 
 // Helper Function to get the market price for a given variant
 const getMarketPrice = (item, variant) => {
-  return item.tcgplayer?.prices?.[variant]?.market || item.tcgplayer?.prices?.[variant]?.mid || 0;
+  return item.tcgplayer?.prices?.[variant]?.market 
+      || item.tcgplayer?.prices?.[variant]?.mid 
+      || 0;
 };
+
 
 const sortByPrice = (cardArray, option = 'high') => {
   let newArray = [...cardArray];
 
   if (option === 'high') {
     newArray.sort((x, y) => {
-      for (let i = 0; i < variants.length; i++) {
-        for (let j = 0; j < variants.length; j++) {
-          if (x.tcgplayer?.prices?.[variants[i]] && y.tcgplayer?.prices?.[variants[j]]) {
-            return getMarketPrice(y, variants[j]) - getMarketPrice(x, variants[i]);
+
+      if (x.tcgplayer?.prices) {
+        if (y.tcgplayer?.prices) {
+
+          for (let i = 0; i < variants.length; i++) {
+            if (x.tcgplayer.prices[variants[i]]) {
+              for (let j = 0; j < variants.length; j++) {
+                if (y.tcgplayer.prices[variants[j]]) {
+                  return getMarketPrice(y, variants[j]) - getMarketPrice(x, variants[i]);
+                }
+              }
+            }
+          }
+          return 0;
+
+        } else if (y.cardmarket?.prices) {
+          
+          for (let i = 0; i < variants.length; i++) {
+            if (x.tcgplayer.prices[variants[i]]) {
+              return y.cardmarket.prices.averageSellPrice - getMarketPrice(x, variants[i]);
+            }
           }
 
+        } else { // y doesn't have price
+          return -1;
         }
-      }
 
-      // If no common variant is found, return 0 (no change in order)
-      return 0;
+
+      } else if (x.cardmarket?.prices) {
+
+        if (y.tcgplayer?.prices) {
+          for (let j = 0; j < variants.length; j++) {
+            if (y.tcgplayer.prices[variants[j]]) {
+              return  getMarketPrice(y, variants[j]) - x.cardmarket.prices.averageSellPrice;
+            }
+          }
+        } else if (y.cardmarket?.prices) {
+          return y.cardmarket.prices.avgSellPrice - x.cardmarket.prices.averageSellPrice;
+        }
+
+
+      } else { // x doesn't have price data
+        return 1;
+      }
     });
   } else { 
     newArray.sort((x, y) => {
-      for (let i = 0; i < variants.length; i++) {
-        for (let j = 0; j < variants.length; j++) {
-          if (x.tcgplayer?.prices?.[variants[i]] && y.tcgplayer?.prices?.[variants[j]]) {
-            return getMarketPrice(x, variants[i]) - getMarketPrice(y, variants[j]);
+      
+      if (x.tcgplayer?.prices) {
+        if (y.tcgplayer?.prices) {
+
+          for (let i = 0; i < variants.length; i++) {
+            if (x.tcgplayer.prices[variants[i]]) {
+              for (let j = 0; j < variants.length; j++) {
+                if (y.tcgplayer.prices[variants[j]]) {
+                  return getMarketPrice(x, variants[i]) - getMarketPrice(y, variants[j]);
+                }
+              }
+            }
           }
+          return 0;
+
+        } else if (y.cardmarket?.prices) {
+          
+          for (let i = 0; i < variants.length; i++) {
+            if (x.tcgplayer.prices[variants[i]]) {
+              return getMarketPrice(x, variants[i]) - y.cardmarket.prices.averageSellPrice;
+            }
+          }
+
+        } else { // y doesn't have price
+          return 1;
         }
+
+
+      } else if (x.cardmarket?.prices) {
+
+        if (y.tcgplayer?.prices) {
+          for (let j = 0; j < variants.length; j++) {
+            if (y.tcgplayer.prices[variants[j]]) {
+              return x.cardmarket.prices.averageSellPrice - getMarketPrice(y, variants[j]);
+            }
+          }
+        } else if (y.cardmarket?.prices) {
+          return x.cardmarket.prices.averageSellPrice - y.cardmarket.prices.avgSellPrice;
+        }
+
+
+      } else { // x doesn't have price data
+        return -1;
       }
 
-      // If no common variant is found, return 0 (no change in order)
-      return 0;
+
     });
   }
   return newArray;
