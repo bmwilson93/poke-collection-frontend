8ba36104688e8c300c8b2349aa9e8a2fbe8d1346
Brian Wilson <bmwilson93@gmail.com>
2026-01-07 04:57:11 -0700

    Updated variant list to reflect changes by scrydex property formats

--- src/components/CardCollectionSection.js
+++ src/components/CardCollectionSection.js
@@ -11,7 +11,7 @@ const CardCollectionSection = ({ card }) => {
 
   // runs the call to the backend to update the collection
   const handleCollectionClick = async (action, variant) => {
-    updateCollection(card.id, card.set.id, action, variant)
+    updateCollection(card.id, card.expansion.id, action, variant)
   }  
 
   // Displays the collected qty
@@ -24,25 +24,42 @@ const CardCollectionSection = ({ card }) => {
     return 0;
   }
 
-  const variantList = Object.keys(card.tcgplayer?.prices || {})
-  .sort((a, b) => variants.indexOf(a) - variants.indexOf(b))
-  .map(key => (
-    <CollectionVariant 
-      key={key}
-      variant={key}
-      handleCollectionClick={handleCollectionClick}
-      displayQuantities={displayQuantities} 
-    />
-  ));
+  // TODO - Update to get the variants from scrydex format
+  // const variantList = Object.keys(card.tcgplayer?.prices || {})
+  // .sort((a, b) => variants.indexOf(a) - variants.indexOf(b))
+  // .map(key => (
+  //   <CollectionVariant 
+  //     key={key}
+  //     variant={key}
+  //     handleCollectionClick={handleCollectionClick}
+  //     displayQuantities={displayQuantities} 
+  //   />
+  // ));
+
+  const variantList = card?.variants
+    ? card.variants
+    .sort((a, b) => variants.indexOf(a) - variants.indexOf(b))
+    .map(variant => (
+      <CollectionVariant
+        key={variant.name}
+        variant={variant.name}
+        handleCollectionClick={handleCollectionClick}
+        displayQuantities={displayQuantities}
+      />
+    ))
+    : [];
+
 
   return (
     <div className="card-collection bottom-border">
       <h2 className='price-header bold'>Your Collection</h2>
-      {/* If card has tcgplayer prices, use them to display card variants */}
-        {card.tcgplayer
-        ? <ul>
-            {variantList}
-          </ul>
+      <p>{card.name}</p>
+      {/* If card has variants, use them to display all card variants */}
+        {card?.variants && card.variants.length > 0
+        ?
+        <ul>
+          {variantList}
+        </ul>
         : <ul>
             {Object.keys({'normal':"", 'holofoil':"", 'reverseHolofoil':""})
             .map(key => (<CollectionVariant 
