061fd21679f1e5843f7094f4958279ec88784fe6
Brian Wilson <bmwilson93@gmail.com>
2026-01-08 17:35:13 -0700

    Added props to access the app sets state. Updated to allow the account page to fetch the sets directly, and added a check to prevent unnecessary requests to scrydex when getting completed sets.

--- src/pages/Account.js
+++ src/pages/Account.js
@@ -5,19 +5,31 @@ import UserContext from '../contexts/UserContext';
 import { fetchAPI } from "../utils/fetchAPI";
 import { getTotalCards, getTotalUniqueCards, getCompletedSets } from '../utils/collectionStats';
 import { getCollectionValue } from '../utils/getCollectionValue';
+import { getSets } from '../utils/fetchData';
 
 import AccountUser from '../components/account/AccountUser';
 import Loading from '../components/Loading';
 
 import './css/Account.css';
 
-const Account = () => {
+const Account = ({ sets, setSets }) => {
   const { user, setUser, checkingUser } = useContext(UserContext);
 
   const navigate = useNavigate();
   const [collectionValue, setCollectionValue] = useState(-1)
   const [calculating, setCalculating] = useState(false)
   const [collectedSets, setcollectedSets] = useState(0)
+
+  const fetchSets = async () => {
+      let response = await getSets();
+  
+      if ('error' in response) {
+        console.log("Error getting the sets");
+      } else {
+        setSets(response);
+        return response;
+      }
+    };
   
 
   // On load, check if logged in
@@ -30,8 +42,14 @@ const Account = () => {
   useEffect(() => {
     const fetchCompletedSets = async () => {
       if (user) {
-        const setsCount = await getCompletedSets(user.collection.sets);
-        setcollectedSets(setsCount); // Set state with the resolved value
+        if (sets.length > 0) {
+          const setsCount = await getCompletedSets(user.collection.sets, sets);
+          setcollectedSets(setsCount); // Set state with the resolved value
+        } else { // No sets current fetched, fetch the sets before running the completed sets
+          const allSets = fetchSets();
+          const setsCount = await getCompletedSets(user.collection.sets, allSets);
+          setcollectedSets(setsCount); // Set state with the resolved value
+        }
       }
     };
   
