750f4ecf2f24c28b02faa1a0807b4c98b7d74773
Brian Wilson <bmwilson93@gmail.com>
2026-01-19 05:01:32 -0700

    Added variant incoming button and functionality, and updated button displays based on collection existance

--- src/components/CollectionVariant.js
+++ src/components/CollectionVariant.js
@@ -1,26 +1,69 @@
 import { formatLabel } from '../utils/formatLabel';
+import mailIcon from '../assets/mail-send-deselect-48.png';
+import selectedMailIcon from '../assets/mail-send-regular-48.png';
 
-const CollectionVariant = ({ variant, handleCollectionClick, displayQuantities }) => {
+const CollectionVariant = ({ variant, handleCollectionClick, displayQuantities, card }) => {
+  const hasCollected = () => {
+    console.log("checking Variant")
+    if (card?.collectedQuantities) {
+      return card?.collectedQuantities.some(e => Object.hasOwn(e, variant)) 
+    }
+    return false
+  }
+
+  const hasIncoming = () => {
+    if (card?.incomingVariants) {
+      return card?.incomingVariants.some(e => Object.hasOwn(e, variant)) 
+    }
+    return false
+  }
+
+  const handleIncomingClick = () => {
+    if (hasIncoming()) {
+      handleCollectionClick('incoming/remove', variant)
+    } else {
+      handleCollectionClick('incoming/add', variant);
+    }
+  }
 
   return (
     <li key={variant} className='li-variant'>
-      <span className='variant'>{formatLabel(variant)}</span>
+      <div className='variant variant-name'>
+        {formatLabel(variant)}
+        {!hasCollected()
+        ? <button 
+            className='incoming-btn'
+            onClick={() => handleIncomingClick()}>
+              <img src={hasIncoming() ? selectedMailIcon : mailIcon}/>
+          </button>
+        : <></>}
+        </div>
       <div>
-        <button
+
+        {
+          hasCollected()
+          ?
+          <button
           className='collection-btn'
           onClick={() => handleCollectionClick('remove', variant)}
-        >
+          >
           -
         </button>
+        :
+        <></>
+        }
+
         <span className='variant variant-total'>
           {displayQuantities(variant)}
         </span>
+
         <button
           className='collection-btn'
           onClick={() => handleCollectionClick('add', variant)}
         >
           +
         </button>
+
       </div>
     </li>
   )
