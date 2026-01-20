315b86ef38a0d3db9ded571ab247ae97ca0a2fda
Brian Wilson <bmwilson93@gmail.com>
2026-01-20 03:00:23 -0700

    Added checks for user and checking user to display error or loading component. Defined a function to get all incoming cards, and display on page

--- src/pages/Incoming.js
+++ src/pages/Incoming.js
@@ -1,16 +1,51 @@
-import { useState, useContext } from 'react';
+import { useState, useContext, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import UserContext from '../contexts/UserContext';
 
 import Loading from '../components/Loading';
 
 const Incoming = () => {
   const { user, setUser, checkingUser } = useContext(UserContext);
+  const navigate = useNavigate();
 
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
+  const [incomingCards, setIncomingCards] = useState([]);
+
+  const getIncomingCards= (collection) => {
+    const incomingCards = [];
+    if (!collection || !collection.sets || !Array.isArray(collection.sets)) {
+      return incomingCards;
+    }
+    collection.sets.forEach(set => {
+      if (set.cards && Array.isArray(set.cards)) {
+        set.cards.forEach(card => {
+          if (card.hasOwnProperty('incoming')) {
+            incomingCards.push(card);
+          }
+        });
+      }
+    });
+    console.log(incomingCards)
+    return incomingCards;
+  }
+
 
   return (
-    <div>Incoming</div>
+    <div className='full-height'>
+      {!user
+        ? checkingUser
+            ? <Loading />
+            : <div>
+                Sign into your account to view your incoming cards.
+                <button onclick={navigate('/login')}>Sign In</button>
+              </div>
+        : <div className="card-list-container">
+            <p>Test</p>
+            <ul className="card-list">
+              {getIncomingCards(user.collection).map(card => (<li><img src={card?.incoming?.card_image}/></li>))}
+            </ul>
+          </div>
+      }
+    </div>
   )
 }
 
