714e5cecdadb71d9e2556a588aa85668d15ab687
Brian Wilson <bmwilson93@gmail.com>
2026-01-20 02:26:59 -0700

    Added loading and error states

--- src/pages/Incoming.js
+++ src/pages/Incoming.js
@@ -1,10 +1,13 @@
-import { useContext } from 'react';
+import { useState, useContext } from 'react';
 import UserContext from '../contexts/UserContext';
 
 import Loading from '../components/Loading';
 
 const Incoming = () => {
   const { user, setUser, checkingUser } = useContext(UserContext);
+
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
   return (
     <div>Incoming</div>
